e1d8e5d0b165394d3ce783a5192c0e3d583e9249
Cedric Geissmann <cedric.geissmann@gmail.com>
2026-02-24 09:11:26 +0100

    Simple Game over.

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ const player = k.add([
 	k.area(),
 	k.body(),
 	k.color(),
+	k.offscreen({ destroy: true }),
 ]);
 k.add([
 	k.rect(640, 20),
@@ -24,3 +25,23 @@ k.add([
 	k.area(),
 	k.body({ isStatic: true }),
 ]);
+
+k.add([
+	k.rect(60, 200),
+	k.pos(580, 260),
+	k.color("green"),
+	k.area(),
+	k.body({ isStatic: true }),
+]);
+
+player.onKeyDown("d", () => {
+	player.moveBy(10, 0);
+});
+
+player.onKeyDown("a", () => {
+	player.moveBy(-10, 0);
+});
+
+player.onDestroy(() => {
+	alert("Game Over");
+});
